4e41c599dbaae4812b7e6d36a136d5b05529af8f
ArJSarmiento <rneljan@gmail.com>
2024-03-29 18:49:55 +0800

    feat: improved views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
